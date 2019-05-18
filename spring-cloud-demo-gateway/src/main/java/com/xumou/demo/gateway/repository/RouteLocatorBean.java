e4fae3949982dfa781b6166e42898e6a1ae93226
ASUS <1643469771@qq.com>
2019-05-18 23:09:03 +0800

    非动态网关

---
+++ spring-cloud-demo-gateway/src/main/java/com/xumou/demo/gateway/repository/RouteLocatorBean.java
@@ -0,0 +1,43 @@
+package com.xumou.demo.gateway.repository;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+// 非动态路由配置
+@Configuration
+public class RouteLocatorBean {
+
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/**") // 拦截路径
+                        .filters(f -> f.filter(filter())) // 过滤器
+                        .uri("lb://WEB-ONE")) // 服务id， lb:// 表示从服务中心获取
+                .build();
+    }
+
+    @Bean
+    GatewayFilter filter(){
+        return new GatewayFilter() {
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                if(1 == 1){
+                    ServerHttpResponse response = exchange.getResponse();
+                    byte[] bytes = "goto error".getBytes();
+                    DataBuffer dataBuffer = response.bufferFactory().wrap(bytes);
+                    return response.writeWith(Flux.just(dataBuffer));
+                }
+                return chain.filter(exchange);
+            }
+        };
+    }
+
+}
