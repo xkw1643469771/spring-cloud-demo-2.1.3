c51b033f65475baf9baa0222fc3ef35eae9e0308
ASUS <1643469771@qq.com>
2019-09-13 01:36:46 +0800

    Jdbc自由数据源使用

--- spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/spring/database/DatabaseTest.java
+++ spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/spring/database/DatabaseTest.java
@@ -1,17 +1,10 @@
 package com.xumou.demo.test.spring.database;
 
 import org.junit.Test;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 public class DatabaseTest {
 
@@ -21,12 +14,72 @@ public class DatabaseTest {
             TestObj test = new TestObj();
             test.setName("Tom");
             test.setNameStr("123123");
-            JdbcUtils.SqlObj sqlObj = JdbcUtils.insertSql(test, "test", JdbcUtils.IGNORE_NULL, JdbcUtils.UPPER_TO_LINE, JdbcUtils.IGNORE("name_str"));
+            SqlUtils.SqlObj sqlObj = SqlUtils.insertSql(test, "test", SqlUtils.IGNORE_NULL, SqlUtils.UPPER_TO_LINE, SqlUtils.IGNORE("nameStr"));
             jdbcTemplate.update(sqlObj.getSql(), sqlObj.getArgs());
             List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from test");
             System.out.println(maps);
             throw new Exception();
         });
+    }
+
+    @Test
+    public void sqlTest(){
+        TestObj test = new TestObj();
+        test.setName("Tom");
+        test.setNameStr("123123");
+        SqlUtils.SqlObj sqlObj = SqlUtils.updateSql(test, "test", column ->  {
+
+        });
+        System.out.println(sqlObj);
+    }
+
+    @Test
+    public void batchSqlTest(){
+        TestObj test = new TestObj();
+        test.setName("Tom");
+        test.setNameStr("123123");
+        SqlUtils.BatchSqlObj sqlObj = SqlUtils.batchUpdateSql( Arrays.asList(test,test), "test",
+                SqlUtils.IGNORE_NULL, SqlUtils.UPPER_TO_LINE, SqlUtils.IGNORE("nameStr"));
+        System.out.println(sqlObj);
+    }
+
+    @Test
+    public void whereSqlTest(){
+        TestObj test = new TestObj();
+        test.setName("Tom");
+        test.setNameStr("123123");
+        SqlUtils.SqlObj sqlObj = SqlUtils.updateSql( test, "test", SqlUtils.IGNORE("nameStr"));
+        System.out.println(sqlObj);
+        sqlObj = SqlUtils.whereSql( test, SqlUtils.IGNORE("nameStr"), column -> {
+            if(column.getFieldName().equals("id")){
+                column.setAndSql("and (id > ? and id < ? )");
+                column.setAndSqlArgs(Arrays.asList(1,2).toArray());
+            }
+        });
+        System.out.println(sqlObj);
+    }
+
+    @Test
+    public void updateWhereSqlTest(){
+        TestObj test = new TestObj();
+        test.setName("Tom");
+        test.setNameStr("123123");
+        SqlUtils.SqlObj test1 = SqlUtils.updateWhereSql(test, "test",
+                SqlUtils.call(column -> {
+                    if(column.getFieldName().equals("id")){
+                        column.setIgnore(true);
+                    }else{
+                        column.setIgnore(false);
+                    }
+                }),
+                SqlUtils.call(column -> {
+                    if(!column.getFieldName().equals("id")){
+                        column.setIgnore(true);
+                    }else{
+                        column.setIgnore(false);
+                    }
+                }), SqlUtils.UPPER_TO_LINE);
+        System.out.println(test1);
     }
 
 }
--- spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/spring/database/JdbcUtils.java
+++ spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/spring/database/JdbcUtils.java
@@ -1,23 +1,18 @@
 package com.xumou.demo.test.spring.database;
 
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.Data;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
-import java.lang.reflect.Field;
-import java.util.LinkedList;
-import java.util.List;
-
 public class JdbcUtils {
 
     public static final String CLASS_NAME = "org.h2.Driver";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "root";
-    public static final String JDBC_URL = "jdbc:h2:d:/h2/test";
+    public static final String PASSWORD = "r";
+    public static final String JDBC_URL = "jdbc:h2:tcp://192.168.88.201:8082/~/test";
 
     private static class Init{
         static JdbcTemplate jdbcTemplate;
@@ -63,123 +58,4 @@ public class JdbcUtils {
         }
     }
 
-    // Sql相关操作  =====================================================================================================
-
-    /** 将大写转为下划线加小写 */
-    public static final ColumnMapper UPPER_TO_LINE = column -> {
-        char[] cs = column.getColumnName().toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (char c : cs) {
-            if(c >= 65 && c <= 90){
-                sb.append("_");
-                c = (char)(c + 32);
-            }
-            sb.append(c);
-        }
-        column.setColumnName(sb.toString());
-    };
-
-    /** 忽略为空的属性 */
-    public static final ColumnMapper IGNORE_NULL = column -> {
-        if (column.getColumnValue() == null)
-            column.setIgnore(true);
-    };
-
-    /** 忽略指定属性 */
-    public static final ColumnMapper IGNORE(String ... name){
-        return column -> {
-            for (String str : name) {
-                if(column.columnName.equals(str))
-                    column.setIgnore(true);
-            }
-        };
-    }
-
-    public static SqlObj updateSql(Object obj, String table, ColumnMapper... columnMappers){
-        StringBuilder sb = new StringBuilder();
-        List<Object> args = new LinkedList<>();
-        sb.append("update ").append(table).append(" set ");
-        sql(obj, column -> {
-            sb.append(column.getColumnName()).append("=").append("?, ");
-            args.add(column.getColumnValue());
-        }, columnMappers);
-        delEnd(sb, ", ");
-        return sqlObj(sb.toString(), args);
-    }
-
-    public static SqlObj insertSql(Object obj, String table, ColumnMapper... columnMappers){
-        StringBuilder sb = new StringBuilder();
-        List<Object> args = new LinkedList<>();
-        sb.append("insert into ").append(table).append(" (");
-        sql(obj, column -> {
-            sb.append(column.getColumnName()).append(", ");
-            args.add(column.getColumnValue());
-        }, columnMappers);
-        delEnd(sb, ", ");
-        sb.append(") values (");
-        for (int i = 0; i < args.size(); i++) {
-            sb.append("?, ");
-        }
-        delEnd(sb, ", ");
-        sb.append(")");
-        return sqlObj(sb.toString(), args);
-    }
-
-    private static SqlObj sqlObj(String sql, List args){
-        SqlObj sqlObj = new SqlObj();
-        sqlObj.setSql(sql);
-        sqlObj.setArgs(args.toArray());
-        return sqlObj;
-    }
-
-    private static void delEnd(StringBuilder sb, String str){
-        String substring = sb.substring(sb.length() - str.length());
-        if(str.equals(substring)){
-            sb.delete(sb.length() - str.length(), sb.length());
-        }
-    }
-
-    private static void sql(Object obj, SqlMapper sqlMapper, ColumnMapper... columnMappers){
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            try {
-                field.setAccessible(true);
-                Column column = new Column();
-                column.setColumnName(field.getName());
-                column.setColumnValue(field.get(obj));
-                for (ColumnMapper columnMapper : columnMappers) {
-                    columnMapper.exec(column);
-                }
-                if(column.ignore){
-                    continue;
-                }
-                sqlMapper.exec(column);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Data
-    public static class SqlObj{
-        private String sql;
-        private Object[] args;
-    }
-
-    @Data
-    public static class Column {
-        private String columnName;
-        private Object columnValue;
-        private boolean ignore;
-    }
-
-    @FunctionalInterface
-    public static interface ColumnMapper {
-        void exec(Column column);
-    }
-
-    @FunctionalInterface
-    public static interface SqlMapper {
-        void exec(Column column);
-    }
 }
