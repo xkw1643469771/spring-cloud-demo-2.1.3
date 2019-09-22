6ed7d03048dc46b96a0c64cb49c7e34c800d15cc
ASUS <1643469771@qq.com>
2019-09-22 19:02:15 +0800

    cache

--- spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/cache/ehcache/EhcacheTest.java
+++ spring-cloud-demo-java-test/src/main/java/com/xumou/demo/test/cache/ehcache/EhcacheTest.java
@@ -4,11 +4,29 @@ import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.ConfigurationFactory;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class EhcacheTest {
 
-    private static CacheManager manager = new CacheManager();
+    private CacheManager manager;
+
+    @Before
+    public void before(){
+//        String path = this.getClass().getClassLoader().getResource("").getFile();
+        String path = "D:/temp/chche/ehcache/test";
+        Configuration configuration = ConfigurationFactory.parseConfiguration();
+        configuration.getDiskStoreConfiguration().setPath(path);
+        manager = CacheManager.create(configuration);
+    }
 
     @Test
     public void test1(){
@@ -27,4 +45,26 @@ public class EhcacheTest {
         System.out.println(cache.get("c"));
     }
 
+    @Test
+    public void test2(){
+        CacheConfiguration conf = new CacheConfiguration();
+        conf.setName("test2");
+        conf.setMaxEntriesLocalHeap(1);     // 内存最大缓存数量
+        conf.setMaxEntriesLocalDisk(0);     // 磁盘最大缓存数量
+        conf.setOverflowToDisk(true);       // 内存达到最大后缓存到磁盘
+        Cache cache = new Cache(conf);
+        manager.addCache(cache);
+        for (int i = 0; i < 10000*100; i++) {
+            cache.put(new Element(i, i));
+        }
+    }
+
+    @Test
+    public void test3() throws IOException {
+        File test2 = manager.getDiskStorePathManager().getFile("test2.data");
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(test2)));
+        for (String i = br.readLine(); i != null; i = br.readLine()) {
+            System.out.println(br.read());
+        }
+    }
 }
